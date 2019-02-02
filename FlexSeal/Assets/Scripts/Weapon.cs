e28e159e2f61c90ebf970ebc27e85815895c868d
EvanStJohn <46731712+EvanStJohn@users.noreply.github.com>
2019-02-01 21:29:55 -0800

    FlexTape
    shootable flextape

---
+++ FlexSeal/Assets/Scripts/Weapon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public KeyCode fire;
+    public float rate;
+    private float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (Input.GetKey(fire) && (timer > rate))
+        {
+            shoot();
+            timer = 0;
+        }
+    }
+
+    void shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
