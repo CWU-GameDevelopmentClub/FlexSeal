ec2ed13385fb281a1357e88cc1da802f3be2c01a
EvanStJohn <46731712+EvanStJohn@users.noreply.github.com>
2019-02-05 17:21:28 -0800

    Bug Fixes
    fixed

--- FlexSeal/Assets/Scripts/Player.cs
+++ FlexSeal/Assets/Scripts/Player.cs
@@ -4,19 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour { 
 
-private Rigidbody2D rb;
-public float speed, jumpForce;
-public KeyCode left, right, jump;
+    private Rigidbody2D rb;
+    public float speed, jumpForce;
+    public KeyCode left, right, jump;
+    private bool isOnGround;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+
         if (Input.GetKey(left))
         {
             rb.velocity = new Vector2(-speed, rb.velocity.y);
@@ -30,9 +34,18 @@ public KeyCode left, right, jump;
             rb.velocity = new Vector2(0, rb.velocity.y);
         }
 
-        if (Input.GetKey(jump))
+        if (Input.GetKey(jump) && isOnGround)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isOnGround = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
         }
     }
 }
